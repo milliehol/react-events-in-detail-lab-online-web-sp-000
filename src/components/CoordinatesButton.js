6b5b8c5fb575aa55f33954040e7559b5d1b248f5
Carla Holcomb <carla.programmer@gmail.com>
2020-10-10 21:48:34 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,13 @@
 // Code CoordinatesButton Component Here
+class CoordinatesButton extends React.Component {
+
+  tickle = () => {
+    console.log('Tee hee!');
+  }
+
+  render() {
+    return (
+      <button onClick={this.tickle}>Tickle me!</button>
+    );
+  }
+}
